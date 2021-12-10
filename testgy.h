888f495c1b3562494ab08035fe0f3545e2b2ec49
ONEMT-DKKAPVJQD\onemt <604306084@qq.com>
2021-12-10 09:49:55 +0800

    test Commit

---
+++ testgy.h
@@ -0,0 +1,17 @@
+#ifndef __testgy__
+#define __testgy__
+
+#include <math.h>
+
+namespace TestGY {
+
+    float Add_GY(float a ; float b)
+    {   
+        float x;
+        x = a + b;
+        return x;
+    }
+
+}
+
+#endif
